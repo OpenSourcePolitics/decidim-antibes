f940a339cb3fac701d7e9a0ff0d83ca8b17a1610
stephanie rousset <strabon@club-internet.fr>
2026-01-15 17:34:43 +0100

    fix: add require logger to fix error in ci

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative "boot"
 
 require "decidim/rails"
 require "action_cable/engine"
+require "logger"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
